77e58061c387d2536f1e50f0d85a41f56c3ca21e
Zyanya <hey.zyanlml@gmail.com>
2024-09-19 16:23:38 -0600

    Updates

---
+++ precache-manifest.0c9f50055d71b87453f300e5193d3324.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b9c5f29088d56ef48d04dc697e56253",
+    "url": "/%7Bweb_project_around_react%7D/index.html"
+  },
+  {
+    "revision": "7687ff2b2ae82aca7e8d",
+    "url": "/%7Bweb_project_around_react%7D/static/css/main.a023d5b3.chunk.css"
+  },
+  {
+    "revision": "417ccf33885c0389b1b8",
+    "url": "/%7Bweb_project_around_react%7D/static/js/2.2704f623.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/%7Bweb_project_around_react%7D/static/js/2.2704f623.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c06a2bdc98bf96c94d3",
+    "url": "/%7Bweb_project_around_react%7D/static/js/3.ef902aff.chunk.js"
+  },
+  {
+    "revision": "7687ff2b2ae82aca7e8d",
+    "url": "/%7Bweb_project_around_react%7D/static/js/main.e99ba89b.chunk.js"
+  },
+  {
+    "revision": "75c432693529d4ae142e",
+    "url": "/%7Bweb_project_around_react%7D/static/js/runtime-main.572d459d.js"
+  },
+  {
+    "revision": "8f7611ae39c682f37516feec261f77dd",
+    "url": "/%7Bweb_project_around_react%7D/static/media/Logo-white.8f7611ae.svg"
+  },
+  {
+    "revision": "dd31dd063f1c376deb28b582c4a64833",
+    "url": "/%7Bweb_project_around_react%7D/static/media/button-heart.dd31dd06.svg"
+  }
+]);
